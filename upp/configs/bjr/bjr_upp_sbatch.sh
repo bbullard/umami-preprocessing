b51bab0ab543aba613e2482f82a348f89231780a
bbullard <brendon.aurele.bullard@cern.ch>
2024-02-06 11:34:46 -0800

    Update UPP bjr configurations for s3df

--- upp/configs/bjr/bjr_upp_sbatch.sh
+++ upp/configs/bjr/bjr_upp_sbatch.sh
@@ -4,8 +4,9 @@
 #SBATCH --job-name=upp-bjr
  
 # choose the GPU queue
-#SBATCH -p usatlas
- 
+#SBATCH --partition=roma
+#SBATCH --account=atlas:usatlas 
+
 # requesting one node
 #SBATCH --nodes=1
 #SBATCH --exclusive
@@ -18,11 +19,13 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --output=/sdf/home/b/bbullard/bjes/umami-preprocessing/upp/configs/sbatch_out/slurm-%j.%x.out
-#SBATCH --error=/sdf/home/b/bbullard/bjes/umami-preprocessing/upp/configs/sbatch_out/slurm-%j.%x.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bbullard@slac.stanford.edu
+#SBATCH --output=/sdf/home/b/bbullard/bjer/datasets/umami-preprocessing/upp/sbatch_out/slurm-%j.%x.out
+#SBATCH --error=/sdf/home/b/bbullard/bjer/datasets/umami-preprocessing/upp/sbatch_out/slurm-%j.%x.err
  
 # move to workdir
-cd /sdf/home/b/bbullard/bjes/umami-preprocessing/upp
+cd /sdf/home/b/bbullard/bjer/datasets/umami-preprocessing/upp
 echo "Moved dir, now in: ${PWD}"
  
 # activate environment
@@ -33,6 +36,6 @@ echo "Activated environment ${CONDA_DEFAULT_ENV}"
 
 # run the training
 echo "Running training script..."
-#srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar_resampled.yaml --split all
-srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar_resampled.yaml --split val
-srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar_resampled.yaml --split test
+srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar.yaml --split all
+#srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar_resampled.yaml --split val
+#srun preprocess --config configs/bjr/bjr_PHYSVAL_ttbar_resampled.yaml --split test
